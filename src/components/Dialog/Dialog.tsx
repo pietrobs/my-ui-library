320f192d447f8353d0bbd310493a91181c3901c6
pietrobs <pietro.schiavinato@unesp.br>
2022-11-23 21:55:19 -0300

    feat(dialog): use hooks in dialog component to close the Dialog with differents ways

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { useRef } from "react";
 import ReactDOM from "react-dom";
+import useClickAwayListener from "../../utils/useClickAwayListener";
+import useKeyPressListener from "../../utils/useKeyPressListener";
 import { DialogWrapper } from "./Dialog.styles";
 import { DialogProps } from "./Dialog.types";
 import DialogHeader from "./DialogHeader";
+
 const Dialog: React.FC<DialogProps> = ({
   children,
   isOpen,
@@ -11,12 +14,21 @@ const Dialog: React.FC<DialogProps> = ({
   closeOnOverlayClick,
   title,
 }) => {
+  const listenerClickAwayRef = useRef<HTMLDivElement>(null);
+
+  useClickAwayListener(
+    listenerClickAwayRef,
+    closeOnOverlayClick ? onClose : undefined
+  );
+
+  useKeyPressListener("Escape", isOpen ? onClose : undefined);
+
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
     <DialogWrapper>
       <div className="dialog-overlay">
-        <div className="dialog">
+        <div className="dialog" ref={listenerClickAwayRef}>
           <DialogHeader title={title} onClose={onClose} />
           <div className="dialog-content">{children}</div>
         </div>
