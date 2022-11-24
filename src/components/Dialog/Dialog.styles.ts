4d48709c76299e7bc31f0c62f537e49bcd961f3f
pietrobs <pietro.schiavinato@unesp.br>
2022-11-23 21:48:54 -0300

    feat(dialog): create dialog structure and styles

---
+++ src/components/Dialog/Dialog.styles.ts
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const DialogWrapper = styled.div`
+  .dialog-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    z-index: 102;
+
+    .dialog {
+      display: flex;
+      flex-direction: column;
+      background-color: #fff;
+      padding: 32px;
+      width: 100%;
+      max-height: 80%;
+
+      .dialog-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+
+        h1 {
+          font-weight: 500;
+          font-size: 22px;
+          line-height: 30px;
+          letter-spacing: -0.032em;
+        }
+
+        button {
+          background-color: transparent;
+          border: none;
+          cursor: pointer;
+          outline: none;
+        }
+      }
+
+      .dialog-content {
+        overflow-y: auto;
+        height: auto;
+
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 24px;
+        letter-spacing: -0.028em;
+
+        ::-webkit-scrollbar {
+          display: none;
+        }
+      }
+    }
+  }
+`;
--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,10 +1,49 @@
 import React from "react";
+import { useRef } from "react";
+import ReactDOM from "react-dom";
+import { DialogWrapper } from "./Dialog.styles";
+import { DialogProps } from "./Dialog.types";
+const Dialog: React.FC<DialogProps> = ({
+  children,
+  isOpen,
+  onClose,
+  closeOnOverlayClick,
+  title,
+}) => {
+  if (!isOpen) return null;
 
-const Dialog = () => {
-  return (
-    <div>
-      <h1>I'm a dialog</h1>
-    </div>
+  return ReactDOM.createPortal(
+    <DialogWrapper>
+      <div className="dialog-overlay">
+        <div className="dialog">
+          <div className="dialog-header">
+            <h1>{title}</h1>
+            <button
+              onClick={() => {
+                onClose();
+              }}
+            >
+              <svg
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M15 1L8 8L15 15" stroke="black" strokeWidth="1.2" />
+                <path
+                  d="M1 1L8 8L0.999999 15"
+                  stroke="black"
+                  strokeWidth="1.2"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="dialog-content">{children}</div>
+        </div>
+      </div>
+    </DialogWrapper>,
+    document.body
   );
 };
 
